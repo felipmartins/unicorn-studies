ed4748be786b90c81f6d8eef042985990e65cfa0
Felps <felipmartins94@gmail.com>
2023-12-05 17:30:24 -0300

    includes app's urls file to projects url

--- config/urls.py
+++ config/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("django_unicorn.urls")),
+    path("", include("task_manager.urls")),
 ]
