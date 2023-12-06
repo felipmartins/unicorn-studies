3429961e53aa6311af71629eec7bb51103eb0a7f
Felps <felipmartins94@gmail.com>
2023-12-06 17:31:43 -0300

    add simple component

--- task_manager/components/tasks.py
+++ task_manager/components/tasks.py
@@ -1,5 +1,12 @@
 from django_unicorn.components import UnicornView
+from task_manager.models import Task
 
 
 class TasksView(UnicornView):
-    pass
+    books = Task.objects.none()
+
+    def mount(self):
+        self.books = Task.objects.all().order_by("-id")[:5]
+
+    def save(self, book_idx: int):
+        self.books[book_idx].save()
